4059f0a6a0ddb1c5008967d4c73ab57f43f7c7ba
Filip Stojanovic <filipstoja@Filips-MacBook-Pro.local>
2022-01-13 17:42:51 -0500

    Updates

--- recommender_app/main.py
+++ recommender_app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import session, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.sql import func
 import json
+import pandas as pd
+import numpy as np
 
 FLASK_SECRET_KEY = '546cfaac2bd3703f257f7a95'
 # Dev PostGres Credentials
@@ -37,7 +39,126 @@ def hello_world():
         movie_counts_subquery, (Movies.id == movie_counts_subquery.c.movieId)).order_by(
         sqlalchemy.desc("movies_count")).all()
     movie_counts = [dict(x) for x in movie_counts]
+
     return render_template('index.html', movies = movie_counts)
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    form_data = request.form
+
+    # Get form data
+    keys = list(form_data.keys())
+    input_ratings = []
+    for x in zip(*form_data.listvalues()):
+        inputs = {}
+        for i in range(len(keys)):
+            inputs[keys[i]] = x[i]
+        input_ratings.append(inputs)
+
+    print(input_ratings)
+
+    validated_ratings = [rating for rating in input_ratings if (rating['movies'] != '-1' and rating['ratings'] != '-1')]
+
+    print(validated_ratings)
+
+    response = {"successful": False}
+    num_valid_ratings = len(validated_ratings)
+
+    if num_valid_ratings < 5:
+        return response
+
+    validated_ratings = [{"movieId": int(x["movies"]), "rating": float(x["ratings"])} for x in validated_ratings]
+    print(validated_ratings)
+
+    ratings = pd.read_sql_table(
+        "ratings",
+        con=db.engine,
+        schema="movie_recommender"
+    )
+
+    ratings_matrix = pd.pivot_table(data=ratings, index='userId', values='rating', columns='movieId')
+
+    movie_ratings = get_nearest_neighbors_prediction(my_ratings = validated_ratings, ratings_matrix = ratings_matrix)
+
+    movies = pd.read_sql_table(
+        "movies",
+        con=db.engine,
+        schema="movie_recommender"
+    )
+    movie_ratings = pd.merge(movie_ratings, movies, left_index = True, right_index = True)[0:40]
+    response['body'] = movie_ratings.to_dict(orient = 'records')
+    print(movie_ratings)
+    response = json.dumps(response)
+    return response
+
+
+def get_nearest_neighbors_prediction(my_ratings, ratings_matrix):
+    min_support = 3
+    max_neighbours = 40
+    my_ratings = pd.DataFrame([{key: value for key, value in y.items() if key in [
+        'movieId', 'rating']} for y in my_ratings]).set_index('movieId')
+
+    my_ratings = my_ratings['rating']
+
+    # Get a list of all movies rated by the user
+    movies_rated = my_ratings.index
+
+    # The total number of movies rated by the user
+    num_movies_rated = len(my_ratings)
+
+    # Only consider the users that are not yourself and the movies that the user has rated
+    ratings_sub_matrix = ratings_matrix.loc[:, movies_rated]
+
+    # Get the number of matching movies rated by other users
+    num_matching_ratings = ratings_sub_matrix.notnull().sum(axis=1)
+
+    # If there are more movies then the minimum support, we want to only consider
+    # The users that have the minimum matching movies
+    if num_movies_rated >= min_support:
+        # Consider only the users greater then the min support
+        matching_users = num_matching_ratings[num_matching_ratings >= min_support].index
+        ratings_sub_matrix = ratings_sub_matrix.loc[matching_users]
+
+    # Get the difference of movie ratings between the user and all other users
+    ratings_diffs = (ratings_sub_matrix - my_ratings) ** 2
+    # Get the Euclidian Distance
+    user_dists = np.sqrt(ratings_diffs.sum(axis=1))
+    # Get similarity score
+    user_sims = 1 / (1 + user_dists);
+
+    user_sims = pd.DataFrame(user_sims, columns=['similarity']).sort_values(
+        by='similarity', ascending=False)
+    most_similar_users = user_sims.iloc[0:max_neighbours]
+
+    # Merge the most similar users and their similarity onto their movie ratings
+    ratings_sub_matrix = pd.merge(ratings_matrix, most_similar_users,
+                                  left_index=True, right_index=True)
+
+    # Remove all movies that have not been rated
+    ratings_sub_matrix = ratings_sub_matrix.dropna(axis=1, how='all')
+
+    movies_to_rate = [x for x in ratings_sub_matrix.columns if x != 'similarity']
+
+    for col in movies_to_rate:
+        # Weight all of the ratings by the user similarity
+        ratings_sub_matrix[col] = ratings_sub_matrix[col] * ratings_sub_matrix['similarity']
+        total_weights = ratings_sub_matrix[ratings_sub_matrix[col].notnull()]['similarity'].sum()
+        ratings_sub_matrix[col] = ratings_sub_matrix[col] / total_weights
+
+    # Get the total number of ratings
+    num_ratings = pd.DataFrame(ratings_sub_matrix[movies_to_rate].notnull().sum(), columns=['num_ratings'])
+    # Get the weighted sum of the ratings
+    movie_ratings = pd.DataFrame(ratings_sub_matrix[movies_to_rate].sum(), columns=['rating'])
+    movie_ratings = pd.merge(movie_ratings, num_ratings, left_index=True, right_index=True)
+
+    # Get the relative confidence score, the more total ratings, the more confident we can be in the score
+    movie_ratings['relative_confidence'] = np.log(1 + movie_ratings['num_ratings'])
+    # Weight the rating with the confidence
+    movie_ratings['weighted_rating'] = movie_ratings['rating'] * movie_ratings['relative_confidence']
+
+    movie_ratings = movie_ratings.sort_values(by='weighted_rating', ascending=False)
+
+    return movie_ratings
 
 class MovieUsers(db.Model):
     id = db.Column("userId", db.Integer(), primary_key=True)
--- recommender_app/static/index.js
+++ recommender_app/static/index.js
@@ -4,7 +4,9 @@ window.onload = function sketch() {
     'genre_children': 'Children', 'genre_comedy': 'Comedy', 'genre_crime': 'Crime', 'genre_documentary': 'Documentary',
     'genre_drama': 'Drama', 'genre_fantasy': 'Fantasy', 'genre_film_noir': 'Noir', 'genre_horror': 'Horror',
     'genre_imax': 'IMAX', 'genre_musical': 'Musical', 'genre_mystery': 'Mystery', 'genre_romance': 'Romance',
-    'genre_sci_fi': 'Sci-Fi', 'genre_thriller': 'Thriller', 'genre_war': 'War', 'genre_western': 'Western'}
+    'genre_sci_fi': 'Sci-Fi', 'genre_thriller': 'Thriller', 'genre_war': 'War', 'genre_western': 'Western'};
+
+    ratings_options = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
 
     // Div for submitting ratings inputs
     var inputRatingsDiv = $("#input__ratings");
@@ -13,6 +15,7 @@ window.onload = function sketch() {
     var addInputRatingBtn = $("#input__rating-button__add");
     addInputRatingBtn.on("click", createRatingsDiv);
 
+    // Create a ratings div
     function createRatingsDiv(){
         // Get current Input Ratings
         var inputRatings = $('.input__rating');
@@ -26,18 +29,21 @@ window.onload = function sketch() {
         }
 
         // Create div for the rating
-        var div = $("<div/>")
-        div.innerHTML = "Rate movie: ";
+        var div = $("<div><br>Rate Movie " + rating_index +": </div>")
         div.addClass("input__rating");
         div.prop("rating_index",rating_index);
         div.addClass('input__rating-index__' + rating_index);
         div.insertBefore(addInputRatingBtn);
 
         // Create a genre selection drop down for the rating
-        var genre_dropdown = $("<select/>");
+        var label = $('<label for="genres">Genre:<br></label>');
+        var genre_dropdown = $('<select class="input__rating-genre__select" form="input-ratings-form" name="genres"/>');
+        var sub_div = $("<div/>");
         genre_dropdown.addClass("input__rating-genre__select");
         genre_dropdown.prop("rating_index", rating_index);
-        div.append(genre_dropdown);
+        sub_div.append(label);
+        sub_div.append(genre_dropdown);
+        div.append(sub_div);
 
         // Add an All genre option
         var option = $("<option/>", {value: "All", text: "All"});
@@ -50,19 +56,22 @@ window.onload = function sketch() {
         }
 
         // Create a movie selection drop down for the rating
-        var movie_dropdown = $("<select/>");
-        movie_dropdown.addClass("input__rating-movie__select");
+        var label = $('<label for="movies">Movie:</label>');
+        var movie_dropdown = $('<select class="input__rating-movie__select" form="input-ratings-form" name="movies"/>');
+        sub_div = $("<div/>");
         movie_dropdown.prop("rating_index", rating_index);
-        div.append(movie_dropdown);
+        sub_div.append(label);
+        sub_div.append(movie_dropdown);
+        div.append(sub_div);
 
         // Add a not seen option
         var options = [{value: -1, text: "None", movies_count: 0}];
 
         // Populate the options with the movies
-        for (var j = 0; j < data.length; j++){
-            var option = {value: data[j].id,
-            text: data[j].title,
-            movies_count: data[j].movies_count}
+        for (const movie of data){
+            var option = {value: movie.id,
+            text: movie.title,
+            movies_count: movie.movies_count}
             options.push(option)
         }
         movie_dropdown.selectize({
@@ -73,6 +82,24 @@ window.onload = function sketch() {
         });
 
         movie_dropdown[0].selectize.addItem(-1);
+
+        // Create a genre selection drop down for the rating
+        var label = $('<label for="ratings">Rating:<br></label>');
+        var rating_dropdown = $('<select class="input__rating-rating__select" form="input-ratings-form" name="ratings"/>');
+        var sub_div = $("<div/>")
+        rating_dropdown.prop("rating_index", rating_index);
+        sub_div.append(label);
+        sub_div.append(rating_dropdown);
+        div.append(sub_div);
+
+        var option = $("<option/>", {value: -1, text: "None"});
+        rating_dropdown.append(option);
+
+        // Populate the genre selection dropdown
+        for (const rating of ratings_options) {
+            var option = $("<option/>", {value: rating, text: rating});
+            rating_dropdown.append(option);
+        }
     }
 
     // Create 5 Ratings Divs
@@ -106,19 +133,19 @@ window.onload = function sketch() {
         var options = [{value: -1, text: "None", movies_count: 0}]
 
         // Iterate through all of the movies filtering by the chosen genre
-        for (var j = 0; j < data.length; j++){
+        for (const movie of data){
             // If the genre is All always add all movies as an option
             if (genre_selected == "All"){
-                var option = {value: data[j].id,
-                text: data[j].title,
-                movies_count: data[j].movies_count}
+                var option = {value: movie.id,
+                text: movie.title,
+                movies_count: movie.movies_count}
                 options.push(option)
             }
             // Otherwise only push items that are matching the genre
-            else if (data[j][genre_selected]){
-                var option = {value: data[j].id,
-                text: data[j].title,
-                movies_count: data[j].movies_count}
+            else if (movie[genre_selected]){
+                var option = {value: movie.id,
+                text: movie.title,
+                movies_count: movie.movies_count}
                 options.push(option)
             }
         }
@@ -130,4 +157,69 @@ window.onload = function sketch() {
         // Try and select the previously selected movie as the Item
         movie_dropdown.addItem(movie_selected);
     }
+
+    var form = $('#input-ratings-form');
+    form.on('submit', async (e) => {
+      e.preventDefault(); // Prevent default form submit behaviour
+
+      var form_data = new FormData(form[0]);
+      var movie_recommendations_div = $('#movie__recommendations');
+      movie_recommendations_div.text("Loading your recommendations...");
+
+      const response = await fetch('/handle_data', {
+        method: 'POST',
+        body: form_data
+      });
+
+      if (response.ok) {
+        // success
+        const response_json = await response.json();
+        console.log(response_json);
+
+        movie_recommendations_div.text("Your Movie Recommendations:");
+        // Create div for the rating
+        var div = $("<div/>");
+        div.addClass("movie__recommendation");
+        div.text("Ayo");
+        movie_recommendations_div.append(div);
+
+        var movie_recommendations = response_json['body'];
+
+        var recommendations_table = $('<table id="movie__recommendations-table"></table>');
+        movie_recommendations_div.append(recommendations_table);
+
+        var table_row = $('<tr/>');
+        recommendations_table.append(table_row);
+
+        var table_col = $('<th>Name</th>');
+        table_row.append(table_col);
+        table_col = $('<th>Match</th>');
+        table_row.append(table_col);
+        table_col = $('<th>URL</th>');
+        table_row.append(table_col);
+
+        for (const movie_recommendation of movie_recommendations.slice(0,20)){
+            table_row = $('<tr/>');
+            recommendations_table.append(table_row);
+
+            table_col = $('<td/>');
+            table_col.text(movie_recommendation.title);
+            table_row.append(table_col);
+
+            table_col = $('<td/>');
+            table_col.text(movie_recommendation.rating/5);
+            table_row.append(table_col);
+
+            table_col = $('<td/>');
+            table_col.text("url");
+            table_row.append(table_col);
+        }
+
+
+
+      } else {
+        // error
+      }
+    }
+    );
 }
